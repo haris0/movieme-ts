47bf332506cd682252271aa4858b90bf3bcfe092
Hari Surrisyad <hari.surrisyad@gmail.com>
2021-12-17 18:09:55 +0700

    improve structure of services

---
+++ services/axios-client.ts
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: 'https://api.themoviedb.org/4/',
+  timeout: 3000,
+  headers: {
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_MDB_TOKEN}`,
+  },
+});
+
+const axiosGet = async (url: string, config?: {}): Promise<any> => {
+  let data = null;
+  let error = null;
+
+  try {
+    const response = await axiosInstance.get(url, config);
+    data = response;
+  } catch (err) {
+    error = err;
+  }
+
+  return { data, error };
+};
+
+export { axiosGet };
--- services/index.ts
+++ services/index.ts
@@ -1,27 +1,5 @@
-import axios from 'axios';
 import { IDiscoverResponse } from 'types';
-
-const axiosInstance = axios.create({
-  baseURL: 'https://api.themoviedb.org/4/',
-  timeout: 3000,
-  headers: {
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_MDB_TOKEN}`,
-  },
-});
-
-const axiosGet = async (url: string, config?: {}): Promise<any> => {
-  let data = null;
-  let error = null;
-
-  try {
-    const response = await axiosInstance.get(url, config);
-    data = response;
-  } catch (err) {
-    error = err;
-  }
-
-  return { data, error };
-};
+import { axiosGet } from './axios-client';
 
 export const getDiscover = async (page:number): Promise<{
   discoverResponse: IDiscoverResponse,

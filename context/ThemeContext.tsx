2fcdbad08ce8012e4b25fa6f55d01fdcc59ef298
Hari Surrisyad <hari.surrisyad@gmail.com>
2021-12-20 11:42:47 +0700

    add theme context

---
+++ context/ThemeContext.tsx
@@ -0,0 +1,47 @@
+/* eslint-disable react/jsx-no-constructed-context-values */
+import {
+  createContext,
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useContext,
+  useState,
+} from 'react';
+
+export interface ThemeContextType{
+  theme : string;
+  setTheme : Dispatch<SetStateAction<string>>
+}
+
+const initialTheme: ThemeContextType = {
+  theme: 'dark',
+  setTheme: () => {},
+};
+
+const ThemeContext = createContext<ThemeContextType>(initialTheme);
+
+type props = {
+  children: ReactNode;
+};
+
+const ThemeContextProvider = ({ children }: props) => {
+  const [theme, setTheme] = useState<string>(initialTheme.theme);
+
+  return (
+    <ThemeContext.Provider value={{
+      theme,
+      setTheme,
+    }}
+    >
+      { children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useSwitchTheme = () => {
+  const { theme, setTheme } = useContext(ThemeContext);
+
+  setTheme(theme === 'dark' ? 'light' : 'dark');
+};
+
+export default ThemeContextProvider;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
+import ContextProvider from 'context';
 
-const MyApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <ContextProvider>
+    <Component {...pageProps} />
+  </ContextProvider>
+);
 
 export default MyApp;

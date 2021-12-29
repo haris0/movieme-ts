76f96a41f23a9e8f69ca86ba06e249c25df5ae47
Hari Surrisyad <hari.surrisyad@gmail.com>
2021-12-29 17:35:56 +0700

    implement select genre

--- components/card/CardSelectGenre.tsx
+++ components/card/CardSelectGenre.tsx
@@ -1,22 +1,35 @@
+/* eslint-disable no-unused-vars */
+import { ChangeEvent } from 'react';
 import { Card } from 'react-bootstrap';
 import { IGenre } from 'types';
 import styles from './CardSelectGenre.module.scss';
 
 type props = {
   theme: 'dark' | 'light';
-  genres: IGenre[];
+  options: IGenre[];
+  selected: string;
+  onChange: (event: ChangeEvent<HTMLSelectElement>) => void;
 };
 
-const CardSelectGenre = ({ theme, genres }: props) => (
+const CardSelectGenre = ({
+  theme,
+  options,
+  selected,
+  onChange,
+}: props) => (
   <Card className={styles[`card_${theme}`]}>
     <Card.Header as="h5">Genre</Card.Header>
     <Card.Body>
       <div style={{ marginBottom: '10px' }}>Filter Results by Genre</div>
       <Card.Text>
-        <select className={`${styles[`select_${theme}`]} ${styles.select_genre}`}>
-          <option>Select Genre</option>
-          {genres.map((genre) => (
-            <option key={genre.id} value={genre.id}>{genre.name}</option>
+        <select
+          className={`${styles[`select_${theme}`]} ${styles.select_genre}`}
+          value={selected}
+          onChange={(event) => onChange(event)}
+        >
+          <option value="">All Genre</option>
+          {options.map((genre) => (
+            <option key={genre.id} value={genre.name.toLowerCase()}>{genre.name}</option>
           ))}
         </select>
       </Card.Text>
--- pages/movie/index.tsx
+++ pages/movie/index.tsx
@@ -2,7 +2,7 @@
 import CardMovie from 'components/card/CardMovie';
 import { useTheme } from 'context/ThemeContext';
 import { GetServerSideProps, NextPage } from 'next';
-import { useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import {
   Col,
   Container,
@@ -12,6 +12,7 @@ import { getDiscover, getGenreList } from 'services';
 import { IGenreListRes, IMovieListRes } from 'types';
 import Link from 'next/link';
 import CardSelectGenre from 'components/card/CardSelectGenre';
+import { useRouter } from 'next/router';
 
 const Movie: NextPage<{
   discoverRes: IMovieListRes
@@ -24,16 +25,42 @@ const Movie: NextPage<{
   genreRes,
   genreErr,
 }) => {
+  const router = useRouter();
+  const { genre } = router.query;
   const theme = useTheme();
 
   const [discoverMovie, setDiscoverMovie] = useState(discoverRes.results);
+  const [selectedGenre, setSelectedGenre] = useState('');
+
+  const handleGenreChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    event.preventDefault();
+    const genreName = event.target.value;
+    setSelectedGenre(genreName);
+    router.push({
+      pathname: '/movie',
+      query: genreName ? { genre: genreName } : {},
+    });
+  };
+
+  useEffect(() => {
+    setDiscoverMovie(discoverRes.results);
+  }, [discoverRes.results]);
+
+  useEffect(() => {
+    setSelectedGenre(genre as string || '');
+  }, [genre]);
 
   return (
     <Container className="container-custome">
       <h3>Discover Movie</h3>
       <Row style={{ marginTop: '1rem' }}>
         <Col sm={3} style={{ marginBottom: '1.5rem' }}>
-          <CardSelectGenre theme={theme} genres={genreRes.genres} />
+          <CardSelectGenre
+            theme={theme}
+            options={genreRes.genres}
+            selected={selectedGenre}
+            onChange={handleGenreChange}
+          />
         </Col>
         <Col sm={9} style={{ marginTop: '-10px' }}>
           <Row>
@@ -58,13 +85,17 @@ const Movie: NextPage<{
   );
 };
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const [discover, genre] = await Promise.all([
-    getDiscover('movie'),
-    getGenreList(),
-  ]);
-  const { discoverRes, discoverErr } = discover;
-  const { genreRes, genreErr } = genre;
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const { genre } = query;
+  const { genreRes, genreErr } = await getGenreList();
+  let genreId = 0;
+
+  if (genre) {
+    const genreObj = genreRes.genres.find((gen) => gen.name.toLowerCase() === genre);
+    genreId = genreObj?.id as number;
+  }
+
+  const { discoverRes, discoverErr } = await getDiscover('movie', +genreId);
 
   return {
     props: {
